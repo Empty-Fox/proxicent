afc4bd8f316647e794acf210476ddd476bcee32a
Empty-Fox <ulyana250188@gmail.com>
2021-04-07 10:58:32 +0300

    update style text first block

--- js/navigateMenuFirstPage.js
+++ js/navigateMenuFirstPage.js
@@ -11,13 +11,12 @@ $(window).scroll(function () {
             $(".nav-menu.w-nav-menu.nav-1 > a").eq(index).addClass("activeMenu");
             $(".nav-menu.w-nav-menu.nav-1 > a").eq(index - 1).removeClass("activeMenu");
             $(".side-nav > a").eq(index - 1).removeClass("w--current");
-            $(".side-nav > a").eq(index).addClass("w--current");
-  
+            $(".side-nav > a").eq(index).addClass("w--current");            
         }
         else {
             $(".side-nav > a").eq(index).removeClass("w--current");
             $(".nav-menu.w-nav-menu.nav-1 > a").eq(index).removeClass("activeMenu");
         }
     })
-   
+  
   });
